952a86486d2b2f2022df410ccc777b869e9ee44b
ayowaaab <ayoub.dahmen@esprit.tn>
2024-03-18 12:05:26 +0100

    🚀 initial Commit

--- src/main/java/tests/Main.java
+++ src/main/java/tests/Main.java
@@ -1,4 +1,5 @@
 package tests;
+
 import entities.Radiologist;
 import entities.User;
 import services.user.RadiologistService;
@@ -9,8 +10,8 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         LocalDate date = LocalDate.parse("2018-05-05");
-        User u1 = new User("warum@es.com","23658",new String[] {"ROLE_USER"},"samir","errouj",date,"male","x");
-        Radiologist r1 = new Radiologist(u1,"22ddd",false);
+        User u1 = new User("warum@es.com", "23658", new String[]{"ROLE_USER"}, "samir", "errouj", date, "male", "x");
+        Radiologist r1 = new Radiologist(u1, "22ddd", false);
         RadiologistService service = new RadiologistService();
 
         try {
@@ -19,6 +20,4 @@ public class Main {
             System.out.println(s.getMessage());
         }
 
-
     }
-}
