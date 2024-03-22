f3513b85d9d67f7ddb7ece4fd98185f5b95c547f
ayowaaab <ayoub.dahmen@esprit.tn>
2024-03-22 15:07:03 +0100

    🌟 LoginPage + Going to unfinished signUp.fxml

---
+++ src/main/java/controllers/user/LoginController.java
@@ -0,0 +1,41 @@
+package controllers.user;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
+
+public class LoginController {
+
+    private Stage stage;
+
+    private Scene scene;
+
+    private Parent root;
+
+
+    @FXML
+    public void signup(ActionEvent event) throws IOException {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/user/signUp.fxml"));
+            stage = (Stage) ((Node) (event.getSource())).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+            Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+            stage.setX((primScreenBounds.getWidth() - stage.getWidth()) / 2);
+            stage.setY((primScreenBounds.getHeight() - stage.getHeight()) / 2);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+
+}
